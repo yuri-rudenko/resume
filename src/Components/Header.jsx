e8ede5849de38176c65577cb54172e0dfa550f9f
Yurii <yurii.rudenko26@gmail.com>
2025-03-15 01:04:41 +0200

    added-left-styles

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
         <header>
             <div className="content">
                 <div className="content-image">
-                    <img src='image' alt='image'></img>
+                    <img src={require('../static/profilepicture.png')} alt='image'/>
                     <div className="header-data">
                         <div className="name">
                             <b>Yuri</b>
@@ -15,13 +15,24 @@ const Header = () => {
                             FRONT END / FULL STACK DEVELOPER
                         </div>
                         <div className="links">
-                            links
+                            <div className="link">
+                                <img src={require('../static/links/github-mark.png')} alt="github"/>
+                                <p>github.com/yuri-rudenko</p>
+                            </div>
+                            <div className="link">
+                                <img src={require('../static/links/Instagram_logo_2022.svg.webp')} alt="insta"/>
+                                <p>instagram.com/dm_dominaria</p>
+                            </div>
+                            <div className="link">
+                                <img src={require('../static/links/Telegram_2019_Logo.svg.webp')} alt=""/>
+                                <p>t.me/yst4rk</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="logo">
-                <img src="logo" alt="logo" />
+                <img src={require('../static/logo.png')} alt="logo" />
             </div>
         </header>
     );
--- src/Components/Left.jsx
+++ src/Components/Left.jsx
@@ -7,47 +7,44 @@ const Left = () => {
             <div className="profile left-section">
                 <b className='paragraph-title'>PROFILE</b>
                 <div className="profile-content">
-                    A Self taught Programmer based in Gujarat, INDIA. Completed Master Degree In Computer Science In Parul University. Skilled Frontend Developer obsessed with product and pixel perfect UI. 3+ Years of Experience in Designing, Building and Maintaining Responsive Websites. Proficient In ReactJS, NextJS, Redux, TailwindCSS, SCSS, Material UI etc.
+                    A Frontend & Full-Stack Developer based in Kyiv, Ukraine, with 1.5 years of hands-on experience building real-world projects. Studied Front-End Development at ITVDN University and specialize in React, Node.js, and modern web technologies.
+
+                    Built a live chat app, sales tracking system, and e-commerce store, focusing on scalability, performance, and user experience. Passionate about UI/UX and building high-quality, efficient applications.
+
+                    Eager to contribute to real-world projects and grow as a developer.
                 </div>
                 <div className="contacts">
-                    <div className='contacts-list'>
-                        <div className='contacts-list-item'>
-                            <img src="" alt="phone" />
-                            <div className="contact">+380 680664444</div>
-                        </div>
-                        <div className='contacts-list-item'>
-                            <img src="" alt="email" />
-                            <div className="contact">yurii.rudenko26@gmail.com</div>
-                        </div>
-                        <div className='contacts-list-item'>
-                            <img src="" alt="location" />
-                            <div className="contact">Kyiv, Uktaine</div>
-                        </div>
+                    <div className='contacts-list-item'>
+                        <img src={require('../static/contacts/phone.png')} alt="phone" />
+                        <div className="contact">+38 0680664400</div>
+                    </div>
+                    <div className='contacts-list-item'>
+                        <img src={require('../static/contacts/mail.png')} alt="email" />
+                        <div className="contact">yurii.rudenko26@gmail.com</div>
+                    </div>
+                    <div className='contacts-list-item'>
+                        <img src={require('../static/contacts/location.png')} alt="location" />
+                        <div className="contact">Kyiv, Uktaine</div>
                     </div>
                 </div>
             </div>
-            <div className="eductation left-section">
+            <div className="education left-section">
                 <b className='paragraph-title'>EDUCATION</b>
                 <div className="education-list">
                     <div className="education-list-item">
-                        <div className="education-date">2021-2024</div>
-                        <div className="education-name">Master of computer science</div>
+                        <div className="education-date">2015-2020</div>
+                        <div className="education-name">Small computer academy</div>
                         <div className="education-location">IT STEP academy</div>
                     </div>
                     <div className="education-list-item">
-                        <div className="education-date">2021-2024</div>
-                        <div className="education-name">Master of computer science</div>
-                        <div className="education-location">IT STEP academy</div>
+                        <div className="education-date">2022-now</div>
+                        <div className="education-name">Artificial intelligence systems and methods</div>
+                        <div className="education-location">Kyiv Polytechnic Institute</div>
                     </div>
                     <div className="education-list-item">
-                        <div className="education-date">2021-2024</div>
-                        <div className="education-name">Master of computer science</div>
-                        <div className="education-location">IT STEP academy</div>
-                    </div>
-                    <div className="education-list-item">
-                        <div className="education-date">2021-2024</div>
-                        <div className="education-name">Master of computer science</div>
-                        <div className="education-location">IT STEP academy</div>
+                        <div className="education-date">2023</div>
+                        <div className="education-name">Frontend developer</div>
+                        <div className="education-location">ITVDN</div>
                     </div>
                 </div>
             </div>
@@ -63,27 +60,27 @@ const Left = () => {
                 <b className='paragraph-title'>HOBBIES</b>
                 <div className="hobbies-list">
                     <div className="hobby">
-                        <img src="" alt="hobby" className="hobby-image" />
+                        <img src={require('../static/hobbies/headphones.png')} alt="hobby" className="hobby-image" />
                         <div className="hobby-name">Listening to Music</div>
                     </div>
                     <div className="hobby">
-                        <img src="" alt="hobby" className="hobby-image" />
+                        <img src={require('../static/hobbies/electric-guitar.png')} alt="hobby" className="hobby-image" />
                         <div className="hobby-name">Guitar</div>
                     </div>
                     <div className="hobby">
-                        <img src="" alt="hobby" className="hobby-image" />
+                        <img src={require('../static/hobbies/popcorn.png')} alt="hobby" className="hobby-image" />
                         <div className="hobby-name">Cinema</div>
                     </div>
                     <div className="hobby">
-                        <img src="" alt="hobby" className="hobby-image" />
+                        <img src={require('../static/hobbies/football.png')} alt="hobby" className="hobby-image" />
                         <div className="hobby-name">Football</div>
                     </div>
                     <div className="hobby">
-                        <img src="" alt="hobby" className="hobby-image" />
+                        <img src={require('../static/hobbies/tennis.png')} alt="hobby" className="hobby-image" />
                         <div className="hobby-name">Tennis</div>
                     </div>
                     <div className="hobby">
-                        <img src="" alt="hobby" className="hobby-image" />
+                        <img src={require('../static/hobbies/board-game.png')} alt="hobby" className="hobby-image" />
                         <div className="hobby-name">Tabletop games</div>
                     </div>
                 </div>
